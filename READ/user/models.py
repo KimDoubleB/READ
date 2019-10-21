e5b466225829a973b7f164229325cb67b6cf27cb
KBB <tree9295@gmail.com>
2019-10-21 23:52:58 +0900

    Add model in user app

--- READ/user/models.py
+++ READ/user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class READ_User(models.Model):
+    username = models.CharField(max_length=128, verbose_name="아이디")
+    password = models.CharField(max_length=128, verbose_name="비밀번호")
+    token = models.CharField(max_length=128)
+    register_date = models.DateTimeField(auto_now_add=True, verbose_name="등록날짜")
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'user' # db 테이블 이름 명명
+        verbose_name = '사용자' # admin 상에서 관리하기 위한 테이블 이름 명명
+        verbose_name_plural = '사용자' # 위와 동일, 복수형시 사용
