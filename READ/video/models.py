e796b9ffec60932f52fee8aa5f2632665eb30151
KimDoubleB <tree9295@gmail.com>
2019-10-28 20:43:23 +0900

    Set model of video app

--- READ/video/models.py
+++ READ/video/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    name = models.CharField(verbose_name="비디오", max_length=256)
+    playlist = models.IntegerField(verbose_name="비디오 개수")
+    description = models.TextField(verbose_name="비디오 설명")
+    register_date = models.DateTimeField(auto_now_add = True, verbose_name="등록날짜")
+    path = models.CharField(max_length=60, null=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'video'              # db table name
+        verbose_name = '비디오'         # db table name in admin
+        verbose_name_plural = '비디오'  # db table name in admin
