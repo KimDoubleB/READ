145980e015b8e14b4c6a0e9795e3c0c82cb3ce9b
KimDoubleB <tree9295@gmail.com>
2019-10-28 20:39:48 +0900

    Add restframework settings (permission)

--- READ/READ/settings.py
+++ READ/READ/settings.py
@@ -121,3 +121,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', )
+}
