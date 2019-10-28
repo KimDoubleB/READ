9a43ded3ef6005fa76b6b73623f98ef8f81f1234
KimDoubleB <tree9295@gmail.com>
2019-10-28 21:11:34 +0900

    Add model of subscribe app

--- READ/subscribe/models.py
+++ READ/subscribe/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Subscribe(models.Model):
+    user = models.ForeignKey('user.READ_User', on_delete=models.CASCADE, verbose_name="사용자")
+    video = models.ForeignKey('video.Video', on_delete=models.CASCADE, verbose_name="비디오")
+    register_date = models.DateTimeField(auto_now_add=True, verbose_name="등록날짜")
+
+
+    def __str__(self):
+        return str(self.user) + ' ' + str(self.video)
+
+
+    class Meta:
+        db_table = 'Subscribe'
+        verbose_name = '구독'
+        verbose_name_plural = '구독'
