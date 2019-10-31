5e9675dd2d3ad2c254ea0222066f21beb0ea08bf
KimDoubleB <tree9295@gmail.com>
2019-10-31 13:06:38 +0900

    Set media directory to upload image/video

--- READ/READ/settings.py
+++ READ/READ/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'user',
     'video',
     'subscribe',
+    'analyzer',
 ]
 
 MIDDLEWARE = [
@@ -124,8 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
