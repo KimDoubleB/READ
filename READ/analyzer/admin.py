f591491795605ce66a6964b8e9a5c4f178592bfe
KBB <tree9295@gmail.com>
2019-11-04 23:24:19 +0900

    Delete reaction model of analyzer

--- READ/analyzer/admin.py
+++ READ/analyzer/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from .models import User_Image, Reaction
+from .models import User_Image
 
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('name', 'cover', )
 
-class ReactionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'video',)
 
 admin.site.register(User_Image, ImageAdmin)
-admin.site.register(Reaction, ReactionAdmin)
--- READ/analyzer/models.py
+++ READ/analyzer/models.py
@@ -6,12 +6,3 @@ class User_Image(models.Model):
     
     def __str__(self):
         return str(self.cover)
-
-class Reaction(models.Model):
-    user = models.ForeignKey('user.READ_User', on_delete=models.CASCADE, verbose_name="사용자")
-    video = models.ForeignKey('video.Video', on_delete=models.CASCADE, verbose_name="비디오")
-    reaction = models.CharField(max_length=512)
-    register_date = models.DateTimeField(auto_now_add=True, verbose_name="등록날짜")
-
-    def __str__(self):
-        return str(self.user) + ' ' + str(self.video)
