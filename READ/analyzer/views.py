5999230d583d5dcbeeca2033cdb123a5a868c3f6
KimDoubleB <tree9295@gmail.com>
2019-12-05 20:40:06 +0900

    Execute face analyzer using thread

--- READ/analyzer/views.py
+++ READ/analyzer/views.py
@@ -1,28 +1,31 @@
-from django.shortcuts import render, redirect
-from .models import User_Image
-from .forms import ImageForm
+from threading import Thread
+
+from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
 
 from user.models import READ_User
 from video.models import Video
-from .models import User_Image
 
 from .face_analyzer import analyze_image
-from threading import Thread
+from .forms import ImageForm
+from .models import User_Image
+
 
 @csrf_exempt
 def image_view(request):
     template ='image.html'
     form = ImageForm(request.POST, request.FILES)
     if form.is_valid():
+
       # get value of form
       image = request.FILES['cover']
       video = Video.objects.get(pk = form.data.get('video'))
       user = READ_User.objects.get(username = form.data.get('user'))
       currentTime = int(float(form.data.get('currentTime')) / 10)
       path = 'images/' + form.data.get('path')
+      duration = form.data.get('duration')
       
-      thread_analyze = Thread(target = analyze_image, args=(user, video, currentTime, path, image, ))
+      thread_analyze = Thread(target = analyze_image, args=(user, video, currentTime, path, image, duration, ))
       thread_analyze.start()
     else:
         print(form)
